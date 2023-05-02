d732be3212fafe8c78c6f371b6025a9e9131be3e
CarloBattista <carlitobatti@gmail.com>
2023-05-02 14:02:34 +0200

    primo sito con vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+    export default{
+        name: "AppTitle",
+    }
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="heading">La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+.heading{
+    font-size: 2rem;
+    font-weight: 700;
+}
+</style>
